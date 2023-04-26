90ab41fa6022c0f73415894d5cacd1531215da65
Muhsineen <77194377+Muhsineen@users.noreply.github.com>
2023-04-26 17:13:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+
+    $(".shoes-colors span").click(function () {
+        $(".shoes-colors span").removeClass("active");
+        $(this).addClass("active");
+        $("body").css("background", $(this).attr("data-color"));
+        $(".shoes-price").css("color", $(this).attr("data-color"));
+        $(".shoes-button").css("color", $(this).attr("data-color"));
+        $(".shoes-pic").css("background-image", $(this).attr("data-pic"));
+    });
+});
+
+
+// =============================change shoe color====================
+
+
